9a4ab1f34747d229e6c5de5bcab23725b0ce312e
Yuci Shen <shen.yuci1@gmail.com>
2021-05-20 16:00:25 -0700

    Complete W2D4 recap exercise 2.

--- W2D4/recap_exercise_2/lib/problems.rb
+++ W2D4/recap_exercise_2/lib/problems.rb
@@ -1,21 +1,44 @@
 # Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
 # of both of the given numbers
 def least_common_multiple(num_1, num_2)
+    smaller = num_1 < num_2 ? num_1 : num_2
+    larger = num_1 > num_2 ? num_1 : num_2
 
+    (larger..larger*smaller).each { |multiple| return multiple if multiple % larger == 0 && multiple % smaller == 0 }
 end
 
 
 # Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
 # most in the string.
 def most_frequent_bigram(str)
+    bigrams = []
+    (0...str.length-1).each { |i| bigrams << str[i..i+1] }
+    return nil if bigrams.empty?
 
+    best = bigrams[0]
+    times = 1
+
+    bigrams.each do |bigram|
+        count = j = 0
+        while temp = str[j..-1].index(bigram)
+            count += 1
+            j += temp + 1
+        end
+        if count > times
+            times = count
+            best = bigram
+        end
+    end
+
+    best
 end
-
 
 class Hash
     # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
-
+        hash = {}
+        self.each { |k, v| hash[v] = k }
+        hash
     end
 end
 
@@ -23,7 +46,15 @@ end
 class Array
     # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
     def pair_sum_count(num)
-
+        count = 0
+        self.each_with_index do |ele, i|
+            j = i + 1
+            while j < self.length
+                count += 1 if self[i] + self[j] == num
+                j += 1
+            end
+        end
+        count
     end
 
     # Write a method, Array#bubble_sort, that takes in an optional proc argument.
@@ -40,6 +71,31 @@ class Array
     #
     # This should remind you of the spaceship operator! Convenient :)
     def bubble_sort(&prc)
+        return self if self.length <= 1
 
+        prc ||= Proc.new do |v1, v2|
+            if v1 < v2
+                -1
+            elsif v1 == v2
+                0
+            else
+                1
+            end
+        end
+
+        swapped = true
+        while swapped
+            swapped = false
+            i = 0
+            while i < self.length - 1
+                if prc.call(self[i], self[i+1]) == 1
+                    swapped = true
+                    self[i], self[i+1] = self[i+1], self[i]
+                end
+                i += 1
+            end
+        end
+
+        return self
     end
 end
