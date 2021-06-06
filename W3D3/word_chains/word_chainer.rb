5d8a5715356adc5afd879b9946665125e4ef5b53
Yuci Shen <shen.yuci1@gmail.com>
2021-06-05 17:24:18 -0700

    Complete W3D3 word_chains exercise.

---
+++ W3D3/word_chains/word_chainer.rb
@@ -0,0 +1,56 @@
+require "byebug"
+
+class WordChainer
+  def initialize(dictionary)
+    @dictionary = File.open("./dictionary.txt")
+    @dictionary = @dictionary.readlines.map(&:chomp)
+  end
+
+  def adjacent_words(word)
+    len = word.length
+    # debugger
+    @dictionary.select do |poss_word|
+      next if poss_word.length != word.length
+      i = 0
+      diff = 0
+      while i < len 
+        diff += 1 if poss_word[i] != word[i]
+        i += 1
+      end
+      diff == 1
+    end
+  end
+
+  def run(source, target)
+    relationships = Hash.new { |h,k| h[k] = [] }
+    all_seen_words = Set.new([source])
+
+    current_words = [source]
+    until current_words.empty?
+      new_word = current_words.shift
+      new_adjacent_words = adjacent_words(new_word)
+
+      new_adjacent_words.each do |word|
+        unless all_seen_words.include?(word)
+          all_seen_words << word
+          relationships[word] << new_word
+          current_words << word
+        end
+      end
+
+      break if all_seen_words.include?(target)
+    end
+
+    raise "No path found" unless all_seen_words.include?(target)
+    build_path(source, target, relationships)
+  end
+
+  def build_path(source, target, relationships)
+    path = [target]
+    until path.last == source
+      path << relationships[path.last].first
+    end
+
+    path.reverse
+  end
+end
