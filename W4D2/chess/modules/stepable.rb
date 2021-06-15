7d64575a4c3f589a43c5e0147f90cc37413d4c32
Yuci Shen <shen.yuci1@gmail.com>
2021-06-14 18:25:11 -0700

    Add stepable module.

---
+++ W4D2/chess/modules/stepable.rb
@@ -0,0 +1,18 @@
+module Stepable
+  def moves
+    valid_moves = []
+    move_diffs.each do |move|
+      x, y = pos
+      x += move[0]
+      y += move[1]
+      
+      next unless x.between?(0, 7) && y.between(0, 7)
+      next if @board[[x,y]].color == @color
+      valid_moves << [x, y]
+    end
+  end
+
+  private
+  def move_diffs #overwritten by subclass
+  end
+end
