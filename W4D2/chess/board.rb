93aaccc53b00c387adf3a2e7461d43259cc419f8
Yuci Shen <shen.yuci1@gmail.com>
2021-06-14 18:52:49 -0700

    Add logic to place pieces onto the board.

--- W4D2/chess/board.rb
+++ W4D2/chess/board.rb
@@ -1,22 +1,48 @@
 require_relative "./pieces/piece"
 
+require_relative "./pieces/bishop"
+require_relative "./pieces/queen"
+require_relative "./pieces/rook"
+
+require_relative "./pieces/pawn"
+require_relative "./pieces/king"
+require_relative "./pieces/knight"
+
+require_relative "./pieces/null_piece"
+
 class Board
   attr_reader :rows
   def initialize
     @rows = Array.new(8) { Array.new(8) }
+    @null_piece = NullPiece.new
+
     self.place_pieces
   end
 
   def place_pieces
     @rows.each_with_index do |row, x|
-      if x == 0
-        row.each_with_index { |spot, y| @rows[x][y] = Piece.new }
-      elsif x == 1
-        row.each_with_index { |spot, y| @rows[x][y] = Piece.new }
-      elsif x == 6
-        row.each_with_index { |spot, y| @rows[x][y] = Piece.new }
-      elsif x == 7
-        row.each_with_index { |spot, y| @rows[x][y] = Piece.new }
+      if x.between?(2,5)
+        row.each_with_index do |spot, y| { @rows[x][y] = @null_piece }
+        next
+      end
+
+      x <= 1 ? row_color = :w : row_color = :B
+      if x == 1 || x == 6
+        row.each_with_index { |spot, y| @rows[x][y] = Pawn.new(row_color, self, [x, y]) }
+      else
+        row.each_with_index do |spot, y|
+          if y == 0 || y == 7
+            rows[x][y] = Rook.new(row_color, self, [x, y])
+          elsif y == 1 || y == 6
+            rows[x][y] = Knight.new(row_color, self, [x, y])
+          elsif y == 2 || y == 5
+            rows[x][y] = Bishop.new(row_color, self, [x, y])
+          elsif y == 3
+            rows[x][y] = Queen.new(row_color, self, [x, y])
+          elsif y == 4
+            rows[x][y] = King.new(row_color, self, [x, y])
+          end
+        end
       end
     end
   end
@@ -52,7 +78,7 @@ class Board
     puts
     symbols = @rows.map do |row|
       row.map do |piece|
-        piece ? "P" : "-"
+        piece.symbol
       end
     end
 
