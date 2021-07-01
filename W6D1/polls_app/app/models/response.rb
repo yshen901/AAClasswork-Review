180514b5fc92003154c8f3763d6d04a8173a060d
Yuci Shen <shen.yuci1@gmail.com>
2021-07-01 15:05:59 -0700

    Fix validation bugs by catching cases where self.id is null.

--- W6D1/polls_app/app/models/response.rb
+++ W6D1/polls_app/app/models/response.rb
@@ -1,7 +1,7 @@
 class Response < ApplicationRecord
   validates :user_id, presence: true
   validates :answer_choice_id, presence: true
-  validates :check_for_previous_response
+  validate :check_for_previous_response
 
   belongs_to :respondent,
     primary_key: :id,
@@ -13,18 +13,23 @@ class Response < ApplicationRecord
     foreign_key: :answer_choice_id,
     class_name: :AnswerChoice
 
-  belongs_to :question,
+  has_one :question,
     through: :answer_choice,
     source: :question
 
   def sibling_responses
-    self.question.responses.where("responses.id != ?", self.id)
+    if self.id
+      self.question.responses.where("responses.id != ?", self.id)
+    else
+      self.question.responses
+    end
   end
 
   def respondent_already_answered?
-    sibling_responses.exists("responses.user_id = ?", self.user_id)
+    sibling_responses.exists?(user_id: self.user_id)
   end
 
+  private
   def check_for_previous_response
     if respondent_already_answered?
       errors[:user_id] << "User has already responded to this question"
