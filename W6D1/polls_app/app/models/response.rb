0aae9e577fc622bad885e7728795541732d7017f
Yuci Shen <shen.yuci1@gmail.com>
2021-07-01 14:36:28 -0700

    Add custom validation preventing user from responding twice to the same question.

--- W6D1/polls_app/app/models/response.rb
+++ W6D1/polls_app/app/models/response.rb
@@ -1,6 +1,7 @@
 class Response < ApplicationRecord
   validates :user_id, presence: true
   validates :answer_choice_id, presence: true
+  validates :check_for_previous_response
 
   belongs_to :respondent,
     primary_key: :id,
@@ -11,4 +12,22 @@ class Response < ApplicationRecord
     primary_key: :id,
     foreign_key: :answer_choice_id,
     class_name: :AnswerChoice
+
+  belongs_to :question,
+    through: :answer_choice,
+    source: :question
+
+  def sibling_responses
+    self.question.responses.where("responses.id != ?", self.id)
+  end
+
+  def respondent_already_answered?
+    sibling_responses.exists("responses.user_id = ?", self.user_id)
+  end
+
+  def check_for_previous_response
+    if respondent_already_answered?
+      errors[:user_id] << "User has already responded to this question"
+    end
+  end
 end
