4ab06345492fd3e7f83905e3f194ef3fe06c3d83
Yuci Shen <shen.yuci1@gmail.com>
2021-07-01 17:58:18 -0700

    Add Question#results that outputs polling results for the question.

--- W6D1/polls_app/app/models/question.rb
+++ W6D1/polls_app/app/models/question.rb
@@ -15,4 +15,41 @@ class Question < ApplicationRecord
   has_many :responses,
     through: :answer_choices,
     source: :responses
+
+  def results_n_plus_one
+    choices = self.answer_choices
+    results = {}
+
+    choices.each do |choice|
+      results[choice.text] = choice.responses.length
+    end
+
+    results
+  end
+
+  def results_wasteful
+    choices = self.answer_choices.includes(:responses)
+    results = {}
+
+    choices.each do |choice|
+      results[choice.text] = choice.responses.length
+    end
+
+    results
+  end
+
+  def results
+    choices = AnswerChoice
+                .left_outer_joins(:responses)
+                .joins(:question)
+                .where("questions.id = ?", self.id)
+                .group("answer_choices.id")
+                .select("COUNT(responses.id) as votes, answer_choices.text as option")
+
+    results = {}
+    choices.each do |choice|
+      results[choice.option] = choice.votes
+    end
+    results
+  end
 end
