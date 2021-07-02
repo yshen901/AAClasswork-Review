6f9d4ff628a7b5cdeeaf2bc7e40c766d6e47751b
Yuci Shen <shen.yuci1@gmail.com>
2021-07-01 22:58:28 -0700

    Complete W6D3 first_routes project.

---
+++ W6D3/first_routes/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save!
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: params
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
--- W6D3/first_routes/db/seeds.rb
+++ W6D3/first_routes/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create!(name: "user_1", email: "user_1@gmail.com")
+User.create!(name: "user_2", email: "user_2@gmail.com")
+User.create!(name: "user_3", email: "user_3@gmail.com")
+User.create!(name: "user_4", email: "user_4@gmail.com")
