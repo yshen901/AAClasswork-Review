328320a2a53609df7b37df80abe587ec63347cda
Yuci Shen <shen.yuci1@gmail.com>
2021-07-05 08:16:28 -0700

    Add associations and validations for comments.

--- W6D4/art_share/app/models/user.rb
+++ W6D4/art_share/app/models/user.rb
@@ -14,4 +14,9 @@ class User < ApplicationRecord
   has_many :shared_artworks,
     through: :artwork_shares,
     source: :artwork
+
+  has_many :comments,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Comment
 end
