aef0bd7bd38fc13f3a55f7fcda6bbd0edf816c2a
Yuci Shen <shen.yuci1@gmail.com>
2021-07-06 15:32:03 -0700

    Add user auth to models and tables.

---
+++ W7D1/cats/app/models/user.rb
@@ -0,0 +1,42 @@
+require 'bcrypt'
+
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, presence: true
+  validates :session_token, presence: true
+
+  validates :password, limit: { minimum: 6, allow_nil: true}
+  validates :password_digest, presence: { message: "Password cannot be blank." }
+
+  before_validation :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def self.find_by_credentials(username, pwd)
+    user = User.find_by(username: username)
+    return nil unless user
+    is_password? ? user : nil 
+  end
+
+  def is_password?(pwd)
+    BCrypt::Password.new(user.password_digest).is_password?(pwd)
+  end
+
+  def password=(pwd)
+    @password = pwd
+  end
+
+  def reset_session_token! 
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+  
+  private
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+end
--- W7D1/cats/db/schema.rb
+++ W7D1/cats/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_07_14_042724) do
+ActiveRecord::Schema.define(version: 2021_07_06_222136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2017_07_14_042724) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "session_token", null: false
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
