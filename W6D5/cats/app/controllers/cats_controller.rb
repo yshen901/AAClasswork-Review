3421f06c919b0e3c05d76abf77786ca084ed6921
Yuci Shen <shen.yuci1@gmail.com>
2021-07-05 18:53:12 -0700

    Place edit and new forms into a partial.

--- W6D5/cats/app/controllers/cats_controller.rb
+++ W6D5/cats/app/controllers/cats_controller.rb
@@ -1,5 +1,4 @@
 class CatsController < ApplicationController
-
   def index
     @cats = Cat.all
     render :index
@@ -12,5 +11,46 @@ class CatsController < ApplicationController
     else
       render json: "Cat not found."
     end
+  end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cats_url(@cat)
+    else
+      render json: @cat.errors.full_messages
+    end
+  end
+
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    if @cat
+      render :edit
+    else
+      render json: "Cat not found."
+    end
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+    if @cat
+      if @cat.update(cat_params)
+        redirect_to cats_url
+      else
+        render json: @cat.errors.full_messages
+      end
+    else
+      render json: "Cat not found."
+    end
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :birth_date, :sex, :description, :color)
   end
 end
