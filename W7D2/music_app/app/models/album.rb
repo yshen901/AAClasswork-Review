334caab99cd7ab7972302b8ba2efdc19f16528b3
Yuci Shen <shen.yuci1@gmail.com>
2021-07-13 08:43:00 -0700

    Fix validation error by replacing presence validation with inclusion for boolean.

--- W7D2/music_app/app/models/album.rb
+++ W7D2/music_app/app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ApplicationRecord
-  validates :title, :year, :live, :band_id, presence: true
+  validates :title, :year, :band_id, presence: true
+  validates_inclusion_of :live, in: [true, false]
   validates :title, uniqueness: {
     scope: :band_id,
     message: "One band cannot have two albums with the same title."
