a6ec7c754897cd58cd710056a7a5059042b70b62
Yuci Shen <shen.yuci1@gmail.com>
2021-07-17 15:22:56 -0700

    Complete W8D3 JS intro part 1.

--- W8D2/js_intro_hw/phase_1.js
+++ W8D2/js_intro_hw/phase_1.js
@@ -1,3 +1,7 @@
+/////////////
+// PHASE 1 //
+/////////////
+
 // var can be redeclared and re-initialized, and is function scope
 function mysteryScoping1() {
   var x = 'out of block';
@@ -49,4 +53,48 @@ function mysteryScoping5() {
   }
   let x = 'out of block again';
   console.log(x);
+}
+
+function madLib(verb, adjective, noun) {
+  return `We shall ${verb.toUpperCase()} the ${adjective.toUpperCase()} ${noun.toUpperCase()}.`
+}
+
+function isSubstring(str, subStr) {
+  return str.indexOf(subStr) != -1
+}
+
+
+/////////////
+// PHASE 2 //
+/////////////
+
+function fizzBuzz(arr) {
+  fizzArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 15 == 0)
+      continue;
+    if (arr[i] % 3 == 0 || arr[i] % 5 == 0)
+      fizzArr.push(arr[i]);
+  }
+  return fizzArr;
+}
+
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0)
+      return false;
+  }
+  return true;
+}
+
+function sumOfNPrimes(n) {
+  let i = 0;
+  let sum = 0;
+  for (let num = 2; i < n; num++) {
+    if (isPrime(num)) {
+      i++;
+      sum += num;
+    }
+  }
+  return sum;
 }
