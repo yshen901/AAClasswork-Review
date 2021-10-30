9e20e7f0024723671bc8edd38cea8fd24ec3d7bb
Yuci Shen <shen.yuci1@gmail.com>
2021-10-30 01:23:57 -0700

    Add ajax request to button to follow/unfollow.

--- W9D4/twitter/frontend/follow_toggle.js
+++ W9D4/twitter/frontend/follow_toggle.js
@@ -4,15 +4,37 @@ export default class FollowToggle {
 
     let data = this.$el.data("follow");
     this.userId = data['user-id'];
-    this.followState = data['initial-follow-state'];
+    this.followed = data['initial-follow-state'];
 
     this.render();
+    this.handleClick();
   }
 
   render() {
-    if (this.followState === 'followed') 
+    if (this.followed) 
       this.$el.text("Unfollow");
     else
       this.$el.text("Follow");
+  }
+
+  handleClick() {
+    this.$el.on("click", event => {
+      event.preventDefault();
+
+      let action = "POST";
+      if (this.followed)
+        action = "DELETE";
+
+      $.ajax({
+        type: action, 
+        url: `${this.userId}/follow`,
+        dataType: 'json',
+        success: () => {
+          console.log("Success!");
+          this.followed = !this.followed;
+          this.render();
+        }
+      });
+    });
   }
 }
