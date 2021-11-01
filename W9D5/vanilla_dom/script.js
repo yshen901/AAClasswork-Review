06c61ab3b961af20411fda516170ed2edf7ab938
Yuci Shen <shen.yuci1@gmail.com>
2021-11-01 15:59:47 -0700

    Complete W9D5 VanillaDOM practice.

--- W9D5/vanilla_dom/script.js
+++ W9D5/vanilla_dom/script.js
@@ -19,13 +19,48 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding SF places as list items
 
   // --- your code here!
+  const input = document.getElementsByClassName("list-container");
+  input[0].addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let place = e.target.children[0].value;
+    document.getElementById("sf-places").innerHTML += `
+      <li>${place}</li>
+    `;
+
+    e.target.children[0].value="";
+  });
 
 
 
   // adding new photos
 
   // --- your code here!
+  const togglePhotoForm = () => {
+    const form = document.getElementsByClassName('photo-form-container')[0];
+    if (form.classList.contains("hidden"))
+      form.classList.remove("hidden");
+    else
+      form.classList.add("hidden");
+  };
 
+  const photoButton = document.getElementsByClassName('photo-show-button')[0];
+  photoButton.addEventListener("click", e => {
+    e.preventDefault();
+    togglePhotoForm();
+  });
 
+  const photoForm = document.getElementsByClassName("photo-form-container")[0];
+  photoForm.addEventListener("submit", e => {
+    e.preventDefault();
 
+    let url = e.target.children[0].value;
+    e.target.children[0].value = "";
+
+    document.getElementsByClassName("dog-photos")[0].innerHTML += `
+      <li>
+        <img src="${url}"></img>
+      </li>
+    `;
+  });
 });
