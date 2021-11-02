f326635d2a235fe58b00a3e157d6002679bf7ddf
Yuci Shen <shen.yuci1@gmail.com>
2021-11-01 17:19:10 -0700

    Complete clock.

--- W9D5/pocket_projects/src/warmup.js
+++ W9D5/pocket_projects/src/warmup.js
@@ -2,6 +2,8 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
+  if (htmlElement.children.length > 0)
+    htmlElement.innerHTML = "";
   htmlElement.innerHTML += `
     <p>${string}</p>
   `;
