1eb1bc30261fbcd4b3de48941b503e73b727bf90
Yuci Shen <shen.yuci1@gmail.com>
2021-11-06 21:57:30 -0700

    Add DOMNodeCollection class object and HTMLElement argument support to core func.

---
+++ W10D1/jquery_lite/src/dom_node_collection.js
@@ -0,0 +1,5 @@
+export default class DOMNodeCollection {
+  constructor(elements) {
+    this.htmlElements = elements;
+  }
+}
--- W10D1/jquery_lite/src/index.js
+++ W10D1/jquery_lite/src/index.js
@@ -1,10 +1,18 @@
-/* 
+import DOMNodeCollection from "./dom_node_collection";
+
+/*
   Core function.
     1. String indicates a CSS selector to identify nodes in the page
     2. Uses Document.querySelectorAll() to make a NodeList
     3. Convert to array then pass it on
 */
 window.$l = (arg) => {
-  elements = document.querySelectorAll(arg);
-  return elementArray = Array.from(elements);
+  if (arg instanceof String) {
+    elements = document.querySelectorAll(arg);
+    elementArray = Array.from(elements);
+    return new DOMNodeCollection(elementArray);
+  }
+  else if (arg instanceof HTMLElement) {
+    return new DOMNodeCollection([arg]);
+  }
 }
