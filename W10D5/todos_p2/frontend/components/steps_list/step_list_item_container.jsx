e36b69b6d992c429c0d254feb4dd08c45e46f99f
Yuci Shen <shen.yuci1@gmail.com>
2021-11-16 20:27:22 -0800

    Add remove/toggle step functionality.

--- W10D5/todos_p2/frontend/components/steps_list/step_list_item_container.jsx
+++ W10D5/todos_p2/frontend/components/steps_list/step_list_item_container.jsx
@@ -1,12 +1,15 @@
 import { connect } from "react-redux";
 import StepListItem from "./step_list_item";
 
+import {receiveStep, removeStep} from "../../actions/steps_actions";
+
 const mapStateToProps = (state, ownProps) => ({
 
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+  receiveStep: (step) => dispatch(receiveStep(step)),
+  removeStep: (step) => dispatch(removeStep(step))
 });
 
 export default connect(
