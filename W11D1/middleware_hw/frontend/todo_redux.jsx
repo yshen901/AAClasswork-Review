08e36ba870ce28034f25d856ebf107514a96bae7
Yuci Shen <shen.yuci1@gmail.com>
2021-11-21 01:03:41 -0800

    Add logger for action, oldState, and newState.

--- W11D1/middleware_hw/frontend/todo_redux.jsx
+++ W11D1/middleware_hw/frontend/todo_redux.jsx
@@ -8,7 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
+  store.dispatch = addLoggingToDispatch(store);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+const addLoggingToDispatch = store => {
+  let originalDispatch = store.dispatch;
+
+  return (action) => {
+    console.log(`Original State: ${JSON.stringify(store.getState())}`);
+    console.log(`Action: ${JSON.stringify(action)}`);
+    originalDispatch(action);
+    console.log(`New State: ${JSON.stringify(store.getState())}`);
+  }
+};
