dbabeaad44dc5bf0ee378babfaa7a2f4896f6e37
Yuci Shen <shen.yuci1@gmail.com>
2021-11-21 01:11:26 -0800

    Refactor logger to use custom applyMiddleWare and middleware signature.

--- W11D1/middleware_hw/frontend/todo_redux.jsx
+++ W11D1/middleware_hw/frontend/todo_redux.jsx
@@ -7,20 +7,47 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
-  store.dispatch = addLoggingToDispatch(store);
+  let store = configureStore(preloadedState);
+  
+  // antipattern! don't override the store.dispatch
+  // store.dispatch = addLoggingToDispatch(store); 
+
+  // better
+  store = applyMiddlewares(store, [addLoggingToDispatch]);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
 
-const addLoggingToDispatch = store => {
-  let originalDispatch = store.dispatch;
+// NOT GOOD: Depends on overwriting store.dispatch which is not good
+// const addLoggingToDispatch = store => {
+//   let originalDispatch = store.dispatch;
 
-  return (action) => {
-    console.log(`Original State: ${JSON.stringify(store.getState())}`);
-    console.log(`Action: ${JSON.stringify(action)}`);
-    originalDispatch(action);
-    console.log(`New State: ${JSON.stringify(store.getState())}`);
+//   return (action) => {
+//     console.log(`Original State: ${JSON.stringify(store.getState())}`);
+//     console.log(`Action: ${JSON.stringify(action)}`);
+//     originalDispatch(action);
+//     console.log(`New State: ${JSON.stringify(store.getState())}`);
+//   }
+// };
+
+// BETTER: Use currying to create the middleware signature
+function addLoggingToDispatch(store) {
+  return function (next) {
+    return function (action) {
+      console.log(`Original State: ${JSON.stringify(store.getState())}`);
+      console.log(`Action: ${JSON.stringify(action)}`);
+      next(action);
+      console.log(`New State: ${JSON.stringify(store.getState())}`);
+    };
+  };
+}
+
+function applyMiddlewares(store, middlewares) {
+  let dispatch = store.dispatch;
+  for (let i = 0; i < middlewares.length; i++) {
+    dispatch = middlewares[i](store)(dispatch);
   }
-};
+
+  return Object.assign({}, store, {dispatch});
+}
