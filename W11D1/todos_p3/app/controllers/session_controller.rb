82d4a6bc27feca16df1442b31b4e819cfa379930
Yuci Shen <shen.yuci1@gmail.com>
2021-11-25 22:47:53 -0800

    Add create/destroy actions to user and session controllers.

--- W11D1/todos_p3/app/controllers/session_controller.rb
+++ W11D1/todos_p3/app/controllers/session_controller.rb
@@ -1,12 +1,38 @@
 class SessionController < ApplicationController
-  before_action :require_no_login
+  # before_action :require_no_login # can't use this since it would invalidate destroy
 
   def new
+    render :new
   end
 
   def create
+    redirect_to root_url if current_user
+
+    @user = User.find_by_credentials(
+      session_params[:username],
+      session_params[:password]
+    )
+
+    if @user
+      login!(@user)
+      redirect_to root_url
+    else
+      flash[:errors] = ["Username/password incorrect!"]
+      redirect_to new_session_url
+    end
   end
 
   def destroy
+    if current_user
+      logout!(current_user)
+    else
+      flash[:errors] = ["Invalid request: no user logged in."]
+    end
+    redirect_to new_session_url
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:username, :password)
   end
 end
--- W11D1/todos_p3/app/controllers/users_controller.rb
+++ W11D1/todos_p3/app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to root_url
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_url
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
