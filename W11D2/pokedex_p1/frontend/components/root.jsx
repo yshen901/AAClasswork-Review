53adf0724ff21a9e2b1d64c4886d38a7f643f2af
Yuci Shen <shen.yuci1@gmail.com>
2021-11-27 02:55:54 -0800

    Add rootReducer, store, Root, and entry file rendering.

---
+++ W11D2/pokedex_p1/frontend/components/root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+const Root = ({store}) => {
+  return (
+    <Provider store={store}>
+
+    </Provider>
+  );
+};
+export default Root;
---
+++ W11D2/pokedex_p1/frontend/store/store.jsx
@@ -0,0 +1,9 @@
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk"
+import rootReducer from "../reducers/root_reducer";
+
+const configureStore = () => {
+  const store = createStore(rootReducer, {}, applyMiddleware(thunk));
+  return store;
+}
+export default configureStore;
