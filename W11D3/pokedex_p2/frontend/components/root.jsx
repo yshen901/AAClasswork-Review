07fd51d44532b9ce00baa968e42e743cc6062107
Yuci Shen <shen.yuci1@gmail.com>
2021-11-27 17:56:19 -0800

    Start W11D3 pokedex_p2 and add HashRouter to tie index to root path.

---
+++ W11D3/pokedex_p2/frontend/components/root.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import PokemonIndexContainer from './../components/pokemon/pokemon_index_container';
+import { HashRouter, Route } from 'react-router-dom';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+      <HashRouter>
+        <App/>
+      </HashRouter>
+  </Provider>
+);
+
+const App = () => (
+  <div>
+    <Route path="/" component={PokemonIndexContainer}></Route>
+  </div>
+)
+
+export default Root;
