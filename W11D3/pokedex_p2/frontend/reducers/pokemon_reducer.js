b1ac793c5626ab505e6ef80b18b2b69fb6f95834
Yuci Shen <shen.yuci1@gmail.com>
2021-11-27 18:44:58 -0800

    Add pokemon detail api, actions, and reducers.

--- W11D3/pokedex_p2/frontend/reducers/pokemon_reducer.js
+++ W11D3/pokedex_p2/frontend/reducers/pokemon_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_ALL_POKEMON } from './../actions/pokemon_actions';
+import { RECEIVE_ALL_POKEMON, RECEIVE_POKEMON } from './../actions/pokemon_actions';
 
 
 const pokemonReducer = (state = {}, action) => {
@@ -6,6 +6,10 @@ const pokemonReducer = (state = {}, action) => {
   switch (action.type){
   case RECEIVE_ALL_POKEMON:
     return Object.assign({}, action.pokemon, state);
+  case RECEIVE_POKEMON:
+    const newState = Object.assign({}, state);
+    newState[action.pokemon.id] = action.pokemon;
+    return newState;
   default:
     return state;
   }
